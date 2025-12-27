bf2cd4ab66b968f012d2da6c1a7f7430051131b4
k1ddy <k1ddy@mail>
2025-12-27 16:51:09 +0500

    Update demo_salon instance_id

--- ops/update_instance_demo.sql
+++ ops/update_instance_demo.sql
@@ -2,7 +2,7 @@ UPDATE clients
 SET config = jsonb_set(
     config,
     '{instance_id}',
-    '"eyJ1aWQiOiJhTFpMend0d1AzUnBCWHpHNlNzbG1aNWNTOTZib1F5YyIsImNsaWVudF9pZCI6InNhbG9uZGVtbyJ9"',
+    '"eyJ1aWQiOiJhTFpMend0d1AzUnBCWHpHNlNzbG1aNWNTOTZib1F5YyIsImNsaWVudF9pZCI6InNhbG9uIn0="',
     true
 )
 WHERE name = 'demo_salon';
